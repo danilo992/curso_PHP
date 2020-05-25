235e91275a8f52e8a73e5bc4c5b67cfab375701a
danilo992 <tadeudanilo46@gmail.com>
2020-05-25 08:19:44 -0300

    aprendendo variáveis variáveis

---
+++ cursophp_01/03-variaveis-variavies.php
@@ -0,0 +1,20 @@
+<?php
+
+$carro = "Fusca";
+
+$$carro = "Amarelo";
+
+echo $carro;
+echo "<br>";
+echo $Fusca;
+echo "<br>";
+
+echo "<hr>";
+
+$local = "Florianópolis";
+$$local = "Santa Catarina";
+
+echo $Florianópolis;
+echo "<br>";
+echo $local;
+
