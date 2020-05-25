5459fe1eee5855b7077dfc7713918686aca812a0
danilo992 <tadeudanilo46@gmail.com>
2020-05-25 08:04:31 -0300

    Criando variáveis

---
+++ cursophp_01/01-basico.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>curso PHP</title>
+</head>
+<body>
+
+<?php
+
+//projeto php
+//curso online
+
+    echo "Meu nome é Danilo</br>";
+    print "Minha idade é 29";
+
+?>
+    
+</body>
+</html>
---
+++ cursophp_01/02-variaves.php
@@ -0,0 +1,22 @@
+<?php
+
+//Meus dados
+$nome = "Danilo Tadeu";
+$idade = "29";
+$altura = "1.80";
+
+echo "Meu nome é $nome,</br> minha idade é $idade</br> e minha altura é $altura. <hr>";
+
+//alterando meus dados
+$nome = "Danilo Silva";
+$idade = "30";
+$altura = "1.81";
+
+print "Meu nome é $nome,</br> minha idade é $idade</br> e minha altura é $altura. <hr>";
+
+//nome validos variáveis
+//$nome
+//$_nome
+//$nome111
+//$nomeSobrenome
+//$nome_sobrenome
